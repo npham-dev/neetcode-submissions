75636076561d75d5b1505d9cb655913dcb88f872
Nathan <146884027+npham-dev@users.noreply.github.com>
2026-04-24 13:54:46 -0700

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.go
@@ -0,0 +1,36 @@
+func removeElement(nums []int, val int) int {
+    // for i := len(nums) - 1; i >= 0; i-- {
+    //     if nums[i] == val {
+    //         nums = slices.Delete(nums, i, i + 1)
+    //     }
+    // }
+    // return len(nums)
+
+    // the above works, but there's a better way that doesn't involve shifting
+    // because we only care about the first k = non-val elements, we can just use swaps instead
+
+    i := 0
+    j := len(nums) - 1
+    
+    for i <= j {
+        // ensure that the end isn't a val
+        for nums[j] == val && j > 0 {
+            j--
+        }
+        if nums[i] == val {
+            temp := nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp 
+            j--
+        }
+        i++
+    }
+
+    // duck-tape this solution so it works 
+    // basically if the first element is a val, pretend like we didn't make an oopsie
+    if i > 0 && nums[0] == val {
+        return 0
+    }
+
+    return i 
+}
