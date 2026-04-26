80f989703efc1736484d339ca894858f6205e767
Nathan <146884027+npham-dev@users.noreply.github.com>
2026-04-25 21:45:10 -0700

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.go
@@ -0,0 +1,11 @@
+func getConcatenation(nums []int) []int {
+    length := len(nums)
+    ans := make([]int, length * 2)
+    for i := 0; i < length; i++ {
+        ans[i] = nums[i]
+    } 
+    for i := 0; i < length; i++ {
+        ans[length + i] = nums[i]
+    }
+    return ans
+}
